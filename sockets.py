043ac6664ee89d9fce3efb19cad8a66052b645c4
fariacc <carolinacfaria94@hotmail.com>
2020-08-11 23:04:13 -0300

    menu de opcoes, recebendo node na rede em multicast e unicast

--- sockets.py
+++ sockets.py
@@ -1,13 +1,18 @@
-import sys
+import os
 import socket
 import threading
 import time
+import pickle
 import Crypto
 from Crypto.PublicKey import RSA
 # from Crypto.Random import get_random_bytes
 # from Crypto.Cipher import AES, PKCS1_OAEP
 
 MULTICAST_ADDR = '224.0.0.1'
+ENVIAR_NOTICIA = 1
+MUDAR_REPUTACAO = 2
+NOVO_NO = 3
+RESPOSTA_NOVO_NO = 4
 BIND_ADDR = '0.0.0.0'
 PORT = 3000
 SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -15,51 +20,65 @@ TIMER = 1
 KEY = RSA.generate(1024)
 PRIVATE_KEY = KEY.export_key()
 PUBLIC_KEY = KEY.publickey().export_key()
+NODES = []
 
+def checkReputation(node):
+    if reputation == 'Confiavel':
+        reputation = 'Nao confiavel'
+        reputation_node = [MUDAR_REPUTACAO, reputation]
+        SOCK.sendto(pickle.dumps(reputation_node), (MULTICAST_ADDR, PORT))
 
 def sendNews():
     print('Escreva a mensagem: ')
     noticia = input()
-    SOCK.sendto(noticia.encode('utf-8'), (MULTICAST_ADDR, PORT))
+    news_node = [ENVIAR_NOTICIA, noticia]
+    SOCK.sendto(pickle.dumps(news_node), (MULTICAST_ADDR, PORT))
 
-def receiveNews():
-    # DEFINIR FORMA DE PRINTAR O IP, A CHAVE PUBLICA E A REPUTACAO QUANDO UM
-    # NÓ CHEGAR, E NAO TODAS AS VEZES QUE MANDAR UMA NOTICIA
+def receiveThread():
+    data_node_sender, address_sender = SOCK.recvfrom(1024)
+    data_node = pickle.loads(data_node_sender)
+    if data_node[0] == NOVO_NO:
+        print("IP do node: ", address_sender[0], ", porta: ", address_sender[1])
+        print("Chave publica do node: ", data_node[1])
+        print("Reputacao do node: ", data_node[2])
+        NODES = [data_node[1], data_node[2], address_sender]
+        your_data = [RESPOSTA_NOVO_NO, PUBLIC_KEY, 'Confiavel']
+        SOCK.sendto(pickle.dumps(your_data), address_sender)
+    elif data_node[0] == RESPOSTA_NOVO_NO:
+        NODES = [data_node[1], data_node[2], address_sender]
+    elif data_node[0] == MUDAR_REPUTACAO:
+        checkReputation()
+    else:
+        print("Noticia: ", data_node[1])
 
-    # news_sender = SOCK.recvfrom(1024)
-    # print("Noticia: ", news_sender[0])
-    public_key_sender, address_sender = SOCK.recvfrom(1024)
-    # reputation_sender = SOCK.recvfrom(1024)
-    print("Chave publica: ", public_key_sender)
-    print("Meu IP: ", address_sender[0])
-    # print("Minha reputacao: ", reputation_sender[0])
-
-    threading.Timer(TIMER, receiveNews).start()
+    threading.Timer(TIMER, receiveThread).start()
 
 def main():
     reputation = 'Confiavel'
+    tipoOperacao = NOVO_NO
     membership = socket.inet_aton(MULTICAST_ADDR) + socket.inet_aton(BIND_ADDR)
     SOCK.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)
     SOCK.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     SOCK.bind((BIND_ADDR, PORT))
 
-    file = open(MULTICAST_ADDR + ".txt", "w")
-    file.writelines([MULTICAST_ADDR + "\n", PUBLIC_KEY.decode('utf-8') + "\n",  reputation + "\n"])
-    file.close()
+    data_node = [tipoOperacao, PUBLIC_KEY, reputation]
+    SOCK.sendto(pickle.dumps(data_node), (MULTICAST_ADDR, PORT))
 
-    SOCK.sendto(PUBLIC_KEY, (MULTICAST_ADDR, PORT))
-    # SOCK.sendto(reputation.encode('utf-8'), (MULTICAST_ADDR, PORT)) # ENVIAR APENAS NA PRIMEIRA VEZ
+    receiveThread()
 
-    receiveNews()
+    print('Opcoes: \n 1 - Enviar noticia \n 2 - Avisar sobre reputacao de node \n 0 - Sair')
+    tipoOperacao = input()
 
-    print('Voce pode enviar uma notícia digitando SIM ou NAO (para sair do programa), ou aguarde receber uma noticia')
-    opcao = input()
-
-    while opcao == 'SIM':
-        sendNews()
-        print('Voce pode enviar uma notícia digitando SIM ou NAO (para sair do programa), ou aguarde receber uma noticia')
-        opcao = input()
-    sys.exit() # POR QUE ESSE DIACHO NAO FINALIZA O PROGRAMA AAAAAA
+    while True:
+        if tipoOperacao == "1":
+            sendNews()
+        elif tipoOperacao == "2":
+            checkReputation()
+        else:
+            break
+        print('Opcoes: \n 1 - Enviar noticia \n 2 - Avisar sobre reputacao de node \n 0 - Sair')
+        tipoOperacao = input()
+    os._exit(0)
 
 if __name__ == '__main__':
     main()
